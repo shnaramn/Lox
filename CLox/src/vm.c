6078b9e0bd0cfcc5b575009e74a80a94169c25f3
Shankar Mathiah Nanjundan <CommandB@outlook.com>
2025-12-26 18:38:26 -0800

    Fix nasty stack bug

--- CLox/src/vm.c
+++ CLox/src/vm.c
@@ -226,7 +226,7 @@ static InterpretResult run() {
                 break;
             }
 
-            //case OP_NIL: push(NIL_VAL); break;
+            case OP_NIL: push(NIL_VAL); break;
             case OP_TRUE: push(BOOL_VAL(true)); break;
             case OP_FALSE: push(BOOL_VAL(false)); break;
 
