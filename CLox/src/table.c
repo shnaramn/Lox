33f6aaf94b7ef86ee6b2b38d626ea83dedafd7da
Shankar Mathiah Nanjundan <CommandB@outlook.com>
2025-12-29 12:13:35 -0800

    Added optimizations

--- CLox/src/table.c
+++ CLox/src/table.c
@@ -20,7 +20,8 @@ void freeTable(Table* table) {
 }
 
 static Entry* findEntry(Entry* entries, int capacity, ObjString* key) {
-    uint32_t index = key->hash % capacity;
+    // uint32_t index = key->hash % capacity;
+    uint32_t index = key->hash & (capacity - 1); // Optimization 1. Capacity is always a power of 2. Bit magic.
     Entry* tombstone = NULL;
 
     for (;;) {
@@ -40,7 +41,7 @@ static Entry* findEntry(Entry* entries, int capacity, ObjString* key) {
             return entry;
         }
 
-        index = (index + 1) % capacity;
+        index = (index + 1) & (capacity - 1);  // Optimization. See note in line 24.
     }
 }
 
@@ -117,7 +118,7 @@ void tableAddAll(Table* from, Table* to) {
 ObjString* tableFindString(Table* table, const char* chars, int length, uint32_t hash) {
     if (table->count == 0) return NULL;
 
-    uint32_t index = hash % table->capacity;
+    uint32_t index = hash & (table->capacity - 1); // Optimization. See note in line 24.
     for (;;) {
         Entry* entry = &table->entries[index];
         if (entry->key == NULL) {
@@ -132,7 +133,7 @@ ObjString* tableFindString(Table* table, const char* chars, int length, uint32_t
             return entry->key;
         }
 
-        index = (index + 1) % table->capacity;
+        index = (index + 1) & (table->capacity - 1); // Optimization. See note in line 24.
     }
 }
 
